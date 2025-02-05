5fd2c1cf7a281e1d7d610f30144914b95c6984ff
nam <tonnnamtipsukanyaja@gmail.com>
2025-02-05 15:02:40 +0700

    add service

--- node-express-user-service/app.js
+++ node-express-user-service/app.js
@@ -14,6 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
