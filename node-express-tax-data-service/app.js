238db7ac2bba8b1600e223a528ba9d58bfdb4d5c
nam <tonnnamtipsukanyaja@gmail.com>
2025-02-25 14:46:43 +0700

    user system

--- node-express-tax-data-service/app.js
+++ node-express-tax-data-service/app.js
@@ -2,20 +2,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/tax-data', usersRouter);
+app.use('/users', usersRouter);
+
 
 var PORT = 8080;
 app.listen(PORT, () => {
--- node-express-tax-data-service/routes/users.js
+++ node-express-tax-data-service/routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
