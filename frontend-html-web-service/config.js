be978b534d758a0058bf0840298a4834cfa145ea
nam <tonnnamtipsukanyaja@gmail.com>
2025-02-27 20:24:34 +0700

    edit env

--- frontend-html-web-service/config.js
+++ frontend-html-web-service/config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-    BASE_PATH: "http://localhost:8080", // Set your base path
+    BASE_PATH: "https://tax2ready-user.achtix.com", // Set your base path
 };
 
 // Attach to the global `window` object so all pages can access it
